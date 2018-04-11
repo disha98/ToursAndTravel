1d036d7d3192c8ff5c841dbb4e872b9c7b525d3d
disha98 <dishajanardhan@hotmail.com>
2018-04-11 15:36:26 +0530

    Create Pkg.java

---
+++ Pkg.java
@@ -0,0 +1,13 @@
+package Tours;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Disha
+ */
+
+public interface packages {
+    abstract void Package()throws IOException;
+    abstract void bill(String a,int b, String c, String d, double e)throws IOException;
+}
